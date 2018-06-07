d14b59dfe1663b5473545b35d928ddda5c4c8b07
tessalynne86 <tessa@tessalynne.com>
2018-06-06 21:42:33 -0400

    Inital Commit
    Inital commit with:
    Bootstrap 3.0.0
    FontAwesome 4.7
    MVC 5.2.3

---
+++ Site Template/Site Template/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Site_Template.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Site Template/Site Template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Site_Template.WebApiApplication" Language="C#" %>
